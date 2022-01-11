414ba55100421e882aee3541a2376ba7557148f6
Eric Fithian <ef2413@columbia.edu>
2022-01-11 12:08:02 -0500

    Updated through part of walkthrough

---
+++ blue_devils.js
@@ -0,0 +1,7 @@
+function time(hours, minutes) {
+    console.log(`It is ${hours}:${minutes}`);
+}
+
+module.exports = {
+    time
+}
---
+++ our_cohort.js
@@ -0,0 +1,4 @@
+let time = require('./blue_devils');
+
+console.log(time);
+time.time(3, 0)
