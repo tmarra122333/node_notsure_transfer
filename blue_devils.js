4b1ad541de5155326416f03e0b856fd4712b1ed8
Eric Fithian <ef2413@columbia.edu>
2022-01-11 15:43:53 -0500

    Updated with most recent code and linked files

--- blue_devils.js
+++ blue_devils.js
@@ -2,6 +2,12 @@ function time(hours, minutes) {
     console.log(`It is ${hours}:${minutes}`);
 }
 
+function counter(num, times) {
+    num += times;
+    console.log(num);
+}
+
 module.exports = {
-    time
+    time,
+    counter
 }
--- our_cohort.js
+++ our_cohort.js
@@ -1,4 +1,16 @@
-let time = require('./blue_devils');
+let duke = require('./blue_devils')
 
-console.log(time);
-time.time(3, 0)
+// console.log(duke);
+
+duke.time(3, 15)
+
+duke.counter(12, 5)
+
+let fs = require('fs');
+let http = require('http');
+
+// console.log(fs)
+
+fs.writeFile('./duke_scores.js', "Nothing here", function() {
+    console.log("This is working")
+})
